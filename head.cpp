c5636d1b25a2831cc3e5b0090e3bbe05a8993f84
xCthulhuFtagn <mkolodyazhny@gmail.com>
2021-09-24 13:55:54 +0300

    TEST alive

---
+++ head.cpp
@@ -0,0 +1,10 @@
+#include "head.h"
+#include "config.h"
+
+#include <iostream>
+
+using namespace std;
+
+void PrintVersion(){
+    cout << "Project version is " << (PROJECT_VERSION_PATCH) << endl;
+}
--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 using namespace std;
 
 int main(){
+    PrintVersion();
     cout << "Hello world!"<<endl;
 }

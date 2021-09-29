41848f4f2c5ec32d2714fc6d7555336f88c65fc9
xCthulhuFtagn <mkolodyazhny@gmail.com>
2021-09-29 15:37:35 +0300

    fixed sort

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,11 @@ int main(){
     ans = SortInput();
     for(auto line : ans){
         for(auto el : line){
-            cout << el << "\t";
+            for (auto i = 0; i < el.size(); ++i){
+                cout << el[i];
+                if(i>0) cout << ".";
+            }
+            cout << '\t';
         }
         cout<<endl;
     }

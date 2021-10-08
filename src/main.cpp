c29cab9bb7da22a5f48b867c7fb951e356b6ae3b
Максим Колодяжный <mkolodyazhny@gmail.com>
2021-10-08 18:06:58 +0300

    working version

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,16 @@ using namespace std;
 int main(){ 
     vector<vector<int>> ans = ConfigureInput();
     sort(ans.rbegin(), ans.rend());
+    cout <<"*********"<<endl;
     auto cond1 = [](const vector<int>& input) {return true;};
     PrintIf(cond1, ans);
+    cout <<"*********"<<endl;
     auto cond2 = [](const vector<int>& input) {return input[0] == 1;};
     PrintIf(cond2, ans);
+    cout <<"*********"<<endl;
     auto cond3 = [](const vector<int>& input) {return input[0] == 46 && input[1] == 70;};
     PrintIf(cond3, ans);
+    cout <<"*********"<<endl;
     auto cond4 = [](const vector<int>& input) {
         return any_of(input.begin(), input.end(), [](const char& x) {return x == 46;});
     };
